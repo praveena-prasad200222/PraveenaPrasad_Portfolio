d1b995347aafd7f5d989594a6a3b64ae3b55a387
praveena-prasad200222 <praveenaprasad611@gmail.com>
2025-12-03 18:44:03 +0530

    resumeee

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3432751493"
+    serviceWorkerVersion: "4227184988"
   }
 });
