0c9e43c4d4f49c5515b6d1d090caf4f989bb12b5
praveena-prasad200222 <praveenaprasad611@gmail.com>
2025-12-03 19:37:27 +0530

    gmail

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2492474135"
+    serviceWorkerVersion: "2519191476"
   }
 });
